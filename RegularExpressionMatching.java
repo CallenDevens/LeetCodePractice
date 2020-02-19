963c42a088e7c03c582da0e977e4f2a64488d48a
KellyZhu <ayarscot@gmail.com>
2020-02-19 20:36:30 +1100

    Problem No 10 Regular Expression Matching
    an intution answer. not correct failed to pass test case "aaa", "a*a"

---
+++ RegularExpressionMatching.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        
+        if(p.startsWith("*")) return false;
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < s.length() && j < p.length() ){
+            char sch = s.charAt(i);
+            char pch = p.charAt(j);
+            if(isEqual(sch, pch)){
+                i++;
+                j++;
+                continue;
+            }else if(pch == '*'){
+                int k = 0;
+                char standChar = p.charAt(j - 1);
+                while (i+k < s.length() && isEqual( s.charAt(i+k), standChar)){
+                    k++;
+                }
+                i = i+k;
+                j++;
+            }else{
+                if(j +1< p.length()&& p.charAt(j+1) == '*'){
+                    j+=2;
+                    continue;
+                }
+                return false;
+            }
+        }
+        
+        return i == s.length() && j == p.length();
+    }
+    
+    public boolean isEqual(char schar, char pchar){
+        return schar == pchar || pchar == '.';
+    }
+}
