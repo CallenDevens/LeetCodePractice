0177f5eee6cfdef5216ee708cf0283fcd78d1a43
Kelly <xiaobing.zhu@yellowfin.bi>
2020-02-20 11:34:20 +1100

    No.10 optimized
    a recusrion solution

---
+++ RegularExpressionMatchingOptimized.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(p.isEmpty()) return s.isEmpty();
+        
+        boolean isFirstCharMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        
+        if(p.length() >= 2 && p.charAt(1) == '*'){
+            //move p by 2, as * needs previous charatcter
+            return isMatch(s, p.substring(2)) ||// zero repeating, can be true even first c not match
+                (isFirstCharMatch && isMatch(s.substring(1), p)); //repeating
+            
+        }else{
+            return isFirstCharMatch && isMatch(s.substring(1), p.substring(1));
+        }
+        
+    }
+}
